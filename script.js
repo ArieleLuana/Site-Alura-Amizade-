deefa65b3c03fc8db897eab9eb902252e4dabe10
ArieleLuana <128522816+ArieleLuana@users.noreply.github.com>
2024-09-06 09:45:52 -0300

    script.js

--- script.js
+++ script.js
@@ -6,82 +6,106 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-        alternativas: [   {
-            texto: "Isso é assustador!",
-            afirmacao: "afirmacao"
+        enunciado: "Você acabou de chegar em uma nova escola onde não conhece ninguém, você prefere?",
+        alternativas: [ {
+            texto: "Tentar se enturmar para criar novas amizades.",
+            afirmacao: "Sou uma pessoa sociável que gosta de conhecer novas pessoas."
         },
         {
-            texto: "Isso é maravilhoso!",
-            afirmacao: "afirmacao"
+            texto: "Ir ficar num canto esperando alguém falar com você.",
+            afirmacao: "Sou uma pessoa antissocial que tem vergonha de conversar com as pessoas"
         }
     ]
 },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
-        alternativas: [   {
-            texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-            afirmacao: "afirmacao"
-        },
-        {
-            texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-            afirmacao: "afirmacao"
-        }
-    ]
+        enunciado: "Após um certo período de aula, você conseguiu se enturmar com o grupo de pessoas que fazem o seu tipo de personalidade. Que são?",
+        alternativas: [
+            {
+                texto: "Pessoas introvertidas, quietas, tímidas e antissociais.",
+                afirmacao: "Com isso, no futuro você se torna uma pessoa mais reservada que tem dificuldades de lidar com situações de extremo contato."
+            },
+            {
+                texto: "Pessoas extrovertidas, animadas, comunicativas e sociáveis.",
+                afirmacao: "Com isso, no futuro você se torna uma pessoa mais afetuosa quem sabe lidar com situações de extremo contato."
+            }
+        ]
 },
-         
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?"
+        enunciado: "No horário de seu intervalo seus colegas te chamam para ir lanchar, o que você responde a eles?"
         ,
-        alternativas: [ {
-            texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-            afirmacao: "afirmacao"
-        },
-        {
-            texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-            afirmacao: "afirmacao"
-        }
-    ]
+        alternativas: [
+            {
+                texto: "Que não quer ir comer porque está sem fome, mas na verdade tem vergonha de comer na frente de desconhecidos.",
+                afirmacao: "Com toda essa vergonha dentro de si, voce fica incapaz de fazer todas as coisas que tem vontade de fazer e as coisas lhe agradam."
+            },
+            {
+                texto: "Que sim e resolve ir com seus novos colegas, e que não se incomoda de comer na frente de desconhecidos.",
+                afirmacao: "Com toda confiança dentro de si, você fica capaz de fazer todas as coisas que tem vontade de fazer e as coisas que lhe agradam."
+            }
+        ]
 },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [ {
-            texto:  "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-            afirmacao: "afirmacao"
-        },
-        {
-            texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-            afirmacao: "afirmacao"
-        }
-    ]
+        enunciado: " Ao voltar para sala de aula percebe que as outras pessoas da sala estou falando e começam a olha para você imediatamente após entrar na sala, o que você faz?",
+        alternativas: [
+            {
+                texto: "Tenta conversar com eles para que não haja nenhum conflito entre você e as outras pessoas da sala.",
+                afirmacao: "Após isso, você descobre que conversando é possível evitar desentendimentos com outras pessoas."
+            },
+            {
+                texto: "Fica assustado ao ver eles falando e olhando para você e acha que estão falando mau.",
+                afirmacao: "Após isso, você evita falar com pessoas que aparentam estar falando sobre você e impede de criar novos laços."
+            }
+        ]
 },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-        alternativas: [ {
-            texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-            afirmacao: "afirmacao"
-        },
-        {
-            texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-            afirmacao: "afirmacao"
-        }
-    ]
+        enunciado: "Depois de um longo dia de aula, sues colegas perguntam se você gostaria de ir embora para casa com eles, o que você faz?",
+        alternativas: [
+            {
+                texto: "Diz que sua casa nao fica na mesma direção da que eles vão para não manter tanto contato com eles.",
+                afirmacao: "Diante disso, você se ve como uma pessoa solitária que não gosta de de socializar que sente que não pode estabelecer uma relação de confiança com as pessoas que convive no seu dia a dia."
+            },
+            {
+                texto: "Diz que seria ótimo, assim conseguindo uma aproximação maior com seus colegas.",
+                afirmacao: "Diante disso,você se ve como uma pessoa auto astral que ama se socializar com as pessoas para poder contar sempre com elas."
+            }
+        ]
 },
+]
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
-function mostraPergunta() {
+function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativa.textContent = alternativa;
-        caixaAlternativas.appendChild(botaoAlternativas);    
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
